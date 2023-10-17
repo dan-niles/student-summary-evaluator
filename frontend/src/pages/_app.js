3475c0b0ca34afc6f5485a1d9ea9e462bd5e903a
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-10-17 10:18:04 +0530

    Set up Clerk auth

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,55 +1,46 @@
-import Head from 'next/head';
-import { CacheProvider } from '@emotion/react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context';
-import { useNProgress } from 'src/hooks/use-nprogress';
-import { createTheme } from 'src/theme';
-import { createEmotionCache } from 'src/utils/create-emotion-cache';
-import 'simplebar-react/dist/simplebar.min.css';
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { AuthConsumer, AuthProvider } from "src/contexts/auth-context";
+import { useNProgress } from "src/hooks/use-nprogress";
+import { createTheme } from "src/theme";
+import { createEmotionCache } from "src/utils/create-emotion-cache";
+import { ClerkProvider } from "@clerk/nextjs";
+import "simplebar-react/dist/simplebar.min.css";
+import "./globals.css";
 
 const clientSideEmotionCache = createEmotionCache();
 
 const SplashScreen = () => null;
 
 const App = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-  useNProgress();
+	useNProgress();
 
-  const getLayout = Component.getLayout ?? ((page) => page);
+	const getLayout = Component.getLayout ?? ((page) => page);
 
-  const theme = createTheme();
+	const theme = createTheme();
 
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>
-          Devias Kit
-        </title>
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width"
-        />
-      </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <AuthProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <AuthConsumer>
-              {
-                (auth) => auth.isLoading
-                  ? <SplashScreen />
-                  : getLayout(<Component {...pageProps} />)
-              }
-            </AuthConsumer>
-          </ThemeProvider>
-        </AuthProvider>
-      </LocalizationProvider>
-    </CacheProvider>
-  );
+	return (
+		<ClerkProvider {...props}>
+			<CacheProvider value={emotionCache}>
+				<Head>
+					<title>Summary Evaluation System</title>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+				</Head>
+				<LocalizationProvider dateAdapter={AdapterDateFns}>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						{getLayout(<Component {...pageProps} />)}
+					</ThemeProvider>
+				</LocalizationProvider>
+			</CacheProvider>
+		</ClerkProvider>
+	);
 };
 
 export default App;
