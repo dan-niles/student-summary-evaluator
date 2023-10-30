c3f2157ecae31b2819e3ef5501a931e32ed25e5b
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-10-30 05:58:49 +0530

    Update middleware to allow API calls

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,7 +1,9 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
 import { clerkClient } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+	ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/(.*)"],
+});
 
 export const config = {
 	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
